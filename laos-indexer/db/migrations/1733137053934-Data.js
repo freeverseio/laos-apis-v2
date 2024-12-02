bcad9894ea947df6201ecd6c06f8d9654ae91c0e
dpla <dpla@freeverse.io>
2024-12-02 17:03:21 +0100

    add rollback migration

--- laos-indexer/db/migrations/1733137053934-Data.js
+++ laos-indexer/db/migrations/1733137053934-Data.js
@@ -7,6 +7,9 @@ module.exports = class Data1733137053934 {
         await db.query(`ALTER TABLE "ownership_contract" ADD COLUMN "bytecode_hash" text`)
     }
 
-    async down(db) {        
+    async down(db) {
+        await db.query(`ALTER TABLE "ownership_contract" DROP COLUMN "name"`)
+        await db.query(`ALTER TABLE "ownership_contract" DROP COLUMN "symbol"`)
+        await db.query(`ALTER TABLE "ownership_contract" DROP COLUMN "bytecode_hash"`)        
     }
 }
